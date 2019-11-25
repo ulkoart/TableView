20a75f7e00936f7f6a9c4b7da4911cca85238c6f
Artem Ulko <ulkoart@gmail.com>
2019-11-25 15:29:14 +0300

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by user on 25/11/2019.
+//  Copyright © 2019 Artem Ulko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
