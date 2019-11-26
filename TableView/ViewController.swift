c5cdcf2b7b280ac2a3cc1432a69fcf9e3353f1ea
Artem Ulko <ulkoart@gmail.com>
2019-11-26 11:50:40 +0300

    init

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    var cellList:[String] = ["Cell 1", "Cell 2", "Cell 3", "Cell 4", "Cell 5", "Cell 6"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = cellList[indexPath.row]
+        cell.detailTextLabel?.text = "detailTextLabel text"
+        
+        if indexPath.row % 2 == 0 {
+            cell.backgroundColor = UIColor.lightGray
+        }
+        
+        return cell
+    }
 
 }
 
